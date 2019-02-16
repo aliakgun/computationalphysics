03db1761e4541a19ba3b3f6e1d95b773d371399d
aliakgun <47610071+aliakgun@users.noreply.github.com>
2019-02-17 02:06:36 +0300

    Update trapez.c
    You can change the function by typing into the function_f. In the future I will change the user interracting algorithm of the program, functions definition of the program for calculating the integral of the function will be dynamically.

--- trapez.c
+++ trapez.c
@@ -24,29 +24,43 @@
 #include <stdio.h>
 
 typedef struct{
-    float a,b,//a and b are the interval of the integral a is the starting point and b is the finishing point.
-            h,//h is the difference between two points on the f(x) function.(f(xi+1)-f(xi)).
-            n;//Amount of the trapezoid
-     integral;//Value of the integral
+    float a,
+          b,//a and b are the interval of the integral a is the starting point and b is the finishing point.
+          h,//h is the difference between two points on the f(x) function.(f(xi+1)-f(xi)).
+          n,//Amount of the trapezoid
+      value;//Value of the value
 }integral_t;
 
 void driver_f(integral_t *integral);//This function interacts with the user
-float function_f();                 //Arguments of the integral
+float function_f(float x);          //Arguments of the integral
 void n_f(integral_t *integral);     //This function calculates amount of the trapezoid
-void integral_f();                  //This function calculates the value of the integral
+float integral_f(integral_t *integral);                  //This function calculates the value of the integral
 
 int main (){
     integral_t integral;
     printf("C implementation of the trapezoidal integration algorithm\n");
+    driver_f(&integral);
+    printf("Integrate of the x*x (a to b) is equal to = %f",integral_f(&integral));
 }
-void driver(integral_t *integral){
+void driver_f(integral_t *integral){
     printf("Please enter the starting interval of the integral>>");
     scanf("%f",&(*integral).a);
     printf("Please enter the finishing interval of the integral>>");
     scanf("%f",&(*integral).b);
-    printf("Please enter the difference between two points on the function(h)");
-    scanf("f",&(*integral).h);
+    printf("Please enter the difference between two points on the function(h)>>");
+    scanf("%f",&(*integral).h);
 }
 void n_f(integral_t *integral){
     (*integral).n=((*integral).b-(*integral).a)/(*integral).h;
+}
+float integral_f(integral_t *integral){
+    n_f(integral);
+    (*integral).value=1/2*((*integral).h)*(function_f((*integral).a)+function_f((*integral).b));
+    for(float i=1;i<(*integral).n;++i){
+        (*integral).value=(*integral).value+((*integral).h)*function_f(i*(*integral).h);
+    }
+    return (*integral).value;
+}
+float function_f(float x){
+    return x;
 }
